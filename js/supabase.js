703c4f5385f71d20ce147b62e718df6ca5c24047
Maria Lombardo <mariialombardo@gmail.com>
2026-02-07 13:26:14 -0300

    migracion a supabase

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = "https://banxdyodqeilqoczbypy.supabase.co"
 const SUPABASE_KEY = "sb_publishable_M52HDFcRVpsXjScBZJ5xNA_xEAkmbsP"
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
